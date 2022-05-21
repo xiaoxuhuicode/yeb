547ef4d36e218e97ffd0c16a423bd9331b730c91
xiaoxuhuicode <2391019497@qq.com>
2022-05-21 22:44:17 +0800

    云e办前端角色操作功能实现

--- src/components/sys/basic/PermissMana.vue
+++ src/components/sys/basic/PermissMana.vue
@@ -1,44 +1,7 @@
-<!--<template>
-  <div>
-    <div class="permissManaTool">
-      <el-input size="small" placeholder="请输入角色英文名" v-model="role.name">
-        <template slot="prepend">ROLE_</template>
-      </el-input>
-      <el-input size="small" placheholder="请输入角色中文名" v-model="role.nameZh" @keydown.enter.native="doAddRole"></el-input>
-      <el-button type="primary" size="small" icon="el-icon-plus" @click="doAddRole">添加角色</el-button>
-    </div>
-    <div class="permissManaMain">
-      <el-collapse v-model="activeName" accordion @change="change">
-        <el-collapse-item :title="r.nameZh" :name="r.id" v-for="(r,index) in roles" :key="index">
-          <el-card class="box-card">
-            <div slot="header" class="clearfix">
-              <span>可访问资源</span>
-              <el-button style="float: right;padding: 3px 0;color: #ff0000" icon="el-icon-delete"
-                         type="text" @click="doDeleteRole(r)"></el-button>
-            </div>
-            <div>
-              <el-tree
-                show-checkbox
-                node-key="id"
-                :key="index"
-                :default-checked-keys="selectedMenus"
-                :data="allMenus" :props="defaultProps"></el-tree>
-              <div style="display: flex;justify-content: flex-end">
-                <el-button @click="cancelUpdate">取消修改</el-button>
-                <el-button type="primary" @click="doUpdate(r.id,index)">确认修改</el-button>
-              </div>
-            </div>
-          </el-card>
-        </el-collapse-item>
-      </el-collapse>
-    </div>
-  </div>
-</template>-->
-
 <template>
   <div>
     <div class="permissManaTool">
-      <el-input size="small" placeholder="请输入角色英文名" vmodel="role.name">
+      <el-input size="small" placeholder="请输入角色英文名" v-model="role.name">
         <template slot="prepend">ROLE_</template>
       </el-input>
       <el-input size="small" v-model="role.nameZh" placeholder="请输入角色中文名"
@@ -109,7 +72,7 @@
             type: 'warning'
           }
         ).then(() => {
-          this.deleteRequest('/system/basic/permission/role/' + role.id).then(resp => {
+          this.deleteRequest('/system/basic/role/role/' + role.id).then(resp => {
             if (resp) {
               this.initRoles();
             }
@@ -123,10 +86,10 @@
       },
       doAddRole() {
         if (this.role.name && this.role.nameZh) {
-          this.postRequest('/system/basic/permission/', this.role).then(resp => {
+          this.postRequest('/system/basic/role/', this.role).then(resp => {
             if (resp) {
-              this.name = '';
-              this.nameZh = '';
+              this.role.name = '';
+              this.role.nameZh = '';
               this.initRoles();
             }
           })
@@ -140,13 +103,14 @@
       doUpdate(rid, index) {
         let tree = this.$refs.tree[index];
         let selectedKeys = tree.getCheckedKeys(true);
-        let url = '/system/basic/permission/?rid=' + rid;
+        let url = '/system/basic/role/?rid=' + rid;
         selectedKeys.forEach(key => {
           url += '&mids=' + key;
         })
         this.putRequest(url).then(resp => {
           if (resp) {
             this.activeName = -1;
+            this.initSelectedMenus(rid);
           }
         })
       },
@@ -158,19 +122,23 @@
       }
       ,
       initAllMenus() {
-        this.getRequest('/system/basic/permission/menus').then(resp => {
-          this.allMenus = resp;
+        this.getRequest('/system/basic/role/meus').then(resp => {
+          if(resp){
+            this.allMenus = resp;
+          }
         })
       }
       ,
       initSelectedMenus(rid) {
-        this.getRequest('/system/basic/permission/mid/' + rid).then(resp => {
-          this.selectedMenus = resp;
+        this.getRequest('/system/basic/role/mid/' + rid).then(resp => {
+          if(resp){
+            this.selectedMenus = resp;
+          }
         })
       }
       ,
       initRoles() {
-        this.getRequest('/system/basic/permission/').then(resp => {
+        this.getRequest('/system/basic/role/').then(resp => {
           if (resp) {
             this.roles = resp;
           }
